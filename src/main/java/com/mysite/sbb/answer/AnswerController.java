854a6ac326e489aa7d5a792cd022c2d41e782706
yunalee2490 <yunalee2490@gmail.com>
2025-11-10 14:49:31 +0900

    3-08 코드 구현 및 테스트 완료

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,5 +1,8 @@
 package com.mysite.sbb.answer;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,8 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mysite.sbb.SecurityConfig;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,17 +23,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class AnswerController {
+
+    private final SecurityConfig securityConfig;
 	private final QuestionService questionService;
 	private final AnswerService answerService;
+	private final UserService userService;
 	
+	@PreAuthorize("isAuthenticated()")
 	@PostMapping("/create/{id}")
-	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
 		Question question = this.questionService.getQuestion(id);
+		SiteUser siteUser = this.userService.getUser(principal.getName());
 		if(bindingResult.hasErrors()) {
 			model.addAttribute("question", question);
 			return "question_detail";
 		}
-		this.answerService.create(question, answerForm.getContent());
+		this.answerService.create(question, answerForm.getContent(), siteUser);
 		return String.format("redirect:/question/detail/%s", id);
 	}
 
